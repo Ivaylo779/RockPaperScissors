d5f77c0c8bbb6068602c89fa885c51b1e62a94da
Ivaylo779 <ivaylobrezin@gmail.com>
2025-12-01 23:58:55 +0200

    finished project

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,28 @@
-rock = "Rock"
-paper = "Paper"
-scissors = "Scissors"
+import random
+
+valid_moves = ['rock', 'paper', 'scissors']
+
+player_move = input("Choose Rock, Paper or Scissors: ").strip().lower()
+
+if player_move not in valid_moves:
+    raise SystemExit("Invalid move. Try again...")
+
+computer_move = random.randint(1, 3)
+
+if computer_move == 1:
+    computer_move = "rock"
+elif computer_move == 2:
+    computer_move = "paper"
+elif computer_move == 3:
+    computer_move = "scissors"
+
+if player_move == 'rock' and computer_move == 'scissors' or \
+        player_move == 'paper' and computer_move == 'rock' or \
+        player_move == 'scissors' and computer_move == 'paper':
+    print("You Won!")
+
+elif player_move == computer_move:
+    print("Draw!")
+
+else:
+    print("You Lost!")
